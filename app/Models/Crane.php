8da949929757335287d4de1bccd92d7111d6ec74
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-25 22:19:41 +0700

    create model crane and company

---
+++ app/Models/Crane.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Crane extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'waranty',
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
