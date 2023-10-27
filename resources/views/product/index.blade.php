57c86945cdc15c1e56b9e2965457e4effc64b496
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-28 00:20:08 +0700

    move card product to index

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,3 +1,17 @@
 <x-app-layout>
-    @livewire('search-product')
+
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('product') }}
+                
+            </h2>
+            @livewire('search-product')
+        </div>
+
+    </x-slot>
+
+    <x-card name="hello" status="loading" price="500" />
+
+   
 </x-app-layout>
