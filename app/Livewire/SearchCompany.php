43cf0f789004a4c853b43f9a58f303b278fef0ab
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-28 23:40:28 +0700

    done crane

--- app/Livewire/SearchCompany.php
+++ app/Livewire/SearchCompany.php
@@ -4,26 +4,21 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Company;
-use App\Models\Repair;
-use App\Models\Task;
+
 
 use Livewire\WithPagination;
-use Livewire\WithFileUploads;
+
 
 
 class SearchCompany extends Component
 {
     use WithPagination;
-    use WithFileUploads;
+
     public $search;
     public $seleted;
     protected $queryString = ['search'];
 
-    public $title;
-    public $details;
-    public $company_id;
-    public $crane_id;
-    public $image;
+
 
 
 
@@ -44,7 +39,8 @@ class SearchCompany extends Component
 
         if ($this->seleted) {
             $selectedCompany = Company::find($this->seleted);
-            $this->company_id = $selectedCompany->id;
+            // $this->company_id = $selectedCompany->id;
+            $this->dispatch('companySelected', $selectedCompany->id);
         }
 
 
@@ -60,36 +56,5 @@ class SearchCompany extends Component
 
         $this->seleted = $company;
         $this->search = '';
-    }
-
-
-    public function save()
-    {
-
-
-        $this->validate([
-            'title' => 'required',
-            'details' => 'required',
-            'company_id' => 'required',
-            // 'crane_id' => 'required',
-            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
-        ]);
-
-
-        // $imageName = time() . '.' . $this->image->extension();
-
-        $this->image->store('repairs');
-
-
-        Repair::create([
-            'name' => $this->title,
-            'description' => $this->details,
-            'company_id' => $this->company_id,
-            'user_id' => auth()->user()->id,
-            'crane_id' => $this->crane_id,
-            'image' => $this->image->hashName(),
-        ]);
-
-        return redirect()->route('show.repair.view');
     }
 }
