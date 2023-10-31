aea576ce12e33a8fbe549bc5e900ec59f4e9900b
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-01 00:05:37 +0700

    edit submit

--- app/Livewire/QuotationCalculate.php
+++ app/Livewire/QuotationCalculate.php
@@ -3,9 +3,6 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use LaravelDaily\Invoices\Invoice;
-use LaravelDaily\Invoices\Classes\Buyer;
-use LaravelDaily\Invoices\Classes\InvoiceItem;
 
 class QuotationCalculate extends Component
 {
@@ -71,22 +68,5 @@ class QuotationCalculate extends Component
     }
     public function checkout()
     {
-        $customer = new Buyer([
-            'name' => 'John Doe',
-            'custom_fields' => [
-                'email' => 'test@example.com',
-            ],
-        ]);
-
-        $item = (new InvoiceItem())->title('Service 1')->pricePerUnit(2);
-
-        $invoice = Invoice::make()
-            ->buyer($customer)
-            ->discountByPercent(10)
-            ->taxRate(15)
-            ->shipping(1.99)
-            ->addItem($item);
-
-        return $invoice->stream();
     }
 }
