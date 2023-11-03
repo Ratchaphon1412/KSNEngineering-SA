145441a107701206ecc6f1ec5e45cd5c9fc95842
kornavit <kornavit.p@ku.th>
2023-11-03 14:50:10 +0700

    flex the blade page

--- resources/views/livewire/search-company.blade.php
+++ resources/views/livewire/search-company.blade.php
@@ -1,56 +1,7 @@
 <div class="h-sreen w-full">
-   <div class="flex flex-row justify-center items-center mx-auto h-full w-full p-12">
-      <div class="flex  items-start w-full gap-2">
-          <div class="w-full  "> 
-            <div class="w-full h-full flex flex-col items-center justify-center">
-               <div class="w-11/12 md:w-8/12 xl:w-1/2 h-auto p-2 rounded-3xl bg-white flex flex-col">
-                  <!-- First section (search bar container) -->
-                  <label for="company" class="mb-2">
-                   <h1 class="text-xs uppercase font-bold tracking-wide  text-gray-700">Search Company</h1>
-                  </label>
-                  <section  class="w-full h-10 flex items-center">
-                     <!-- Search icon container -->
-                     <span class="w-10 h-full hidden md:flex items-center">
-                        <i class="uil uil-search-alt text-xl text-blue-800"></i>
-                     </span>
-                     <!-- Input -->
-                     <input id="company"  wire:model.live="search" type="search"  class="w-full h-full font-medium md:pl-2 focus:outline-none searchInput rounded-lg px-4 mx-4"
-                        placeholder="Search what you want ...">
-                     <!-- Search button -->
-                    
-                  </section>
-                  <!-- Second section (results container) -->
-                  <section class="w-full h-auto  flex-col gap-y-2 mt-8 resultsContainer">
-       
-                     @foreach ($items as $item )
-                  
-                        <div class="w-full h-10 flex items-center gap-x-2 hover:border border-gray-400 rounded-lg p-4" wire:click.live="selectItem({{$item->id}})" wire:key="{{$item->id}}" >
-                              <!-- Image container -->
-                              <span class="w-7 h-7">
-                                 <img src="{{$item->logo}}" alt="">
-                              </span>
-                              <p class="font-medium text-base text-black">{{$item->name}}</p>
-                              
-                        </div>
-                        
-                     @endforeach
-                     
-                     @if($items)
-                        {{ $items->links() }}
-                     
-                     @endif
-
-
-                  
-                  
-                  </section>
-
-               </div>
-            </div>
-          </div>
-          <div class="w-full">
-           
-              <form class="w-full max-w-lg" wire:submit.prevent="save" >
+   <div class="flex flex-col justify-center items-center mx-auto h-full w-full p-12">
+      <div class="flex justify-center w-full">
+         <form class="w-full bg-white p-10 rounded-lg max-w-lg" wire:submit.prevent="save" >
                
                   <div class="flex flex-wrap -mx-3 mb-6">
                   <div class="w-full md:w-5/6 px-3 mb-6 md:mb-0">
@@ -106,7 +57,7 @@
              
                   <div class="w-full  px-3 mb-6 md:mb-0">
                      <label  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                      Company
+                     Search Company
                      </label>
                      @if($selectedCompany)
                     
@@ -114,11 +65,56 @@
                         <livewire:card-company :company="$selectedCompany"  wire:key="{{$selectedCompany}}"/>
                         <input type="text" class="hidden" wire:model="company_id"  wire:key="{{$selectedCompany->id}}"  >
                      @else
-                     <div class="card card-side  shadow-xl">
-                        
-                        <div class="card-body">
-                           <p class="text-black text-md ">Please select company</p>
+                     <div>
+                     {{-- start of search --}}
+                     <div class="w-full"> 
+                        <div class="w-full h-full flex flex-col items-center justify-center">
+                           <div class="w-full h-auto p-2 rounded-3xl bg-gray-100 flex flex-col">
+                              <!-- First section (search bar container) -->
+                              <label for="company" class="mb-2">
+                              <!-- <h1 class="text-xs uppercase font-bold tracking-wide  text-gray-700">Search Company</h1> -->
+                              </label>
+                              <section  class="w-full h-10 flex items-center">
+                                 <!-- Search icon container -->
+                                 <span class="w-10 h-full hidden md:flex items-center justify-end">
+                                    <i class="uil uil-search-alt text-xl text-blue-800"></i>
+                                 </span>
+                                 <!-- Input -->
+                                 <input id="company"  wire:model.live="search" type="search"  class="w-full h-full font-medium md:pl-2 focus:outline-none searchInput rounded-lg px-4 mx-4"
+                                    placeholder="Search what you want ...">
+                                 <!-- Search button -->
+                              
+                              </section>
+                              <!-- Second section (results container) -->
+                              <section class="w-full h-auto  flex-col gap-y-2 mt-8 resultsContainer">
+                  
+                                 @foreach ($items as $item )
+                              
+                                    <div class="w-full h-10 flex items-center gap-x-2 hover:border border-gray-400 rounded-lg p-4" wire:click.live="selectItem({{$item->id}})" wire:key="{{$item->id}}" >
+                                          <!-- Image container -->
+                                          <span class="w-7 h-7">
+                                             <img src="{{$item->logo}}" alt="">
+                                          </span>
+                                          <p class="font-medium text-base text-gray-500">{{$item->name}}</p>
+                                          
+                                    </div>
+                                    
+                                 @endforeach
+                                 
+                                 @if($items)
+                                    {{ $items->links() }}
+                                 
+                                 @endif
+
+
+                              
+                              
+                              </section>
+
+                           </div>
                         </div>
+                     </div>
+                     {{-- end of search --}}
                         @error('company_id')
                            <p class="text-red-500 text-xs italic">{{$message}}</p>
                         @enderror
@@ -155,9 +151,6 @@
                  
 
               </form>
-     
-  
-          </div>
       </div>
      </div>
 
