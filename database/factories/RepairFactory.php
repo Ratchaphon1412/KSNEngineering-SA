865cdd49e8fc7865628075644c5d28988f2395f1
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-08 06:00:55 +0700

    create RepairFactory and Seeder

--- database/factories/RepairFactory.php
+++ database/factories/RepairFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Repair>
@@ -17,12 +18,18 @@ class RepairFactory extends Factory
      */
     public function definition(): array
     {
+        $file_path = Storage::disk('public')->path('repairs');
+        $image = $this->faker->image($file_path, 640, 480);
+        $fileName = basename($image);
+
+
         return [
             'company_id' => \App\Models\Company::all()->random()->id,
             'user_id' => \App\Models\User::all()->random()->id,
             'crane_id' => \App\Models\Crane::all()->random()->id,
             'name' => $this->faker->name(),
             'description' => fake()->text(100),
+            'image' => $fileName,
         ];
     }
 }
