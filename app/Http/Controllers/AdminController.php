a4e47dbfe7f8cd1d014dc6760b1eca2e238beb0a
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-18 18:02:11 +0700

    fetch transection to dashboard admin and total money in system

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Product;
+use App\Service\Payments\Omise;
+use App\Models\Payment;
 
 class AdminController extends Controller
 {
@@ -11,7 +13,16 @@ class AdminController extends Controller
     {
         // $users = collect();
         $users = User::where('name', '!=', 'admin')->paginate(5);
+        $count_users = User::where('name', '!=', 'admin')->count();
         $products = Product::paginate(10);
+        $count_products = Product::count();
+        $payments = Omise::AdminBalance();
+        $payments = $payments->toArray();
+
+        $transections = Payment::paginate(15);
+
+
+
         // foreach($tmpusers as $user){
         //     if($user->roles->contains('name','user')){
         //         $users->push($user);
@@ -19,8 +30,12 @@ class AdminController extends Controller
         // }
 
         return view('admin.dashboard', [
+            'count_users' => $count_users,
+            'count_products' => $count_products,
+            'payments' => $payments['total'] / 100,
             'users' => $users,
-            'products' => $products
+            'products' => $products,
+            'transections' => $transections,
         ]);
     }
 
